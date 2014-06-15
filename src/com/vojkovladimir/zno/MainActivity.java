a226a940318e864763a06d393190e3ad5939cc3d
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-06-15 18:55:50 +0300

    Initial commit

---
+++ src/com/vojkovladimir/zno/MainActivity.java
@@ -0,0 +1,64 @@
+package com.vojkovladimir.zno;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+
+	protected void onStart() {
+		super.onStart();
+	}
+
+	protected void onStop() {
+		super.onStop();
+	}
+
+	protected void onRestart() {
+		super.onRestart();
+	}
+
+	protected void onPause() {
+		super.onPause();
+	}
+
+	protected void onResume() {
+		super.onResume();
+	}
+
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+	}
+
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+	}
+
+	public void beginTesting(View v) {
+		
+	}
+
+	public void lastPassedTests(View v) {
+
+	}
+
+	public void records(View v) {
+
+	}
+
+	public void settings(View v) {
+
+	}
+
+}
