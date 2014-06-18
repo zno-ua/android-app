d254e6a6ca6959cf462323f60dbde451cd9fb175
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-06-18 22:36:03 +0300

    Testing Activity simplified

--- src/com/vojkovladimir/zno/TestingActivity.java
+++ src/com/vojkovladimir/zno/TestingActivity.java
@@ -1,20 +1,61 @@
 package com.vojkovladimir.zno;
 
-import android.app.Activity;
+import android.app.ListActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.widget.ListView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 
-public class TestingActivity extends Activity{
-	
+import com.vojkovladimir.zno.db.ZNODataBaseHelper;
+
+public class TestingActivity extends ListActivity {
+
 	public static String LOG_TAG = "MyLogs";
 
-	ListView lessonsList;
+	ArrayAdapter<String> lessonsListAdapter;
+	int[] lessonsIds;
+
+	OnItemClickListener itemListener = new OnItemClickListener() {
+		@Override
+		public void onItemClick(AdapterView<?> parent, View v, int position,
+				long id) {
+			try{
+				Log.d(LOG_TAG, "You select lesson with id: "+lessonsIds[position]);
+			}catch(ArrayIndexOutOfBoundsException e){
+				Log.e(LOG_TAG, "You try to select lesson #"+position+" from list.");
+			}
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_testing);
-		
-		lessonsList = (ListView)findViewById(R.id.lessons_list_view);
+
+		lessonsListAdapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1);
+
+		setListAdapter(lessonsListAdapter);
+		getListView().setOnItemClickListener(itemListener);
+
+		SQLiteDatabase db = ZNOApplication.getInstance().getZnoDataBaseHelper()
+				.getReadableDatabase();
+
+		Cursor c = db.query(ZNODataBaseHelper.TABLE_LESSONS_LIST, null, null,
+				null, null, null, null);
+		lessonsIds = new int[c.getCount()];
+
+		if (c.moveToFirst()) {
+			int lessonIdIndex = c.getColumnIndex(ZNODataBaseHelper.KEY_ID);
+			int lessonNameIndex = c.getColumnIndex(ZNODataBaseHelper.KEY_NAME);
+
+			for (int i = 0; i < lessonsIds.length; i++, c.moveToNext()) {
+				lessonsListAdapter.add(c.getString(lessonNameIndex));
+				lessonsIds[i] = c.getInt(lessonIdIndex);
+			}
+		}
 	}
 }
