ed565ab228710369bd1079e51abb62149f40aa71
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-06-24 00:54:42 +0300

    Lesson structure added

---
+++ src/com/vojkovladimir/zno/models/Lesson.java
@@ -0,0 +1,14 @@
+package com.vojkovladimir.zno.models;
+
+public class Lesson {
+
+	public String name;
+	public int testsCount;
+	
+	public Lesson(String name,int testsCount) {
+		this.name = name;
+		this.testsCount = testsCount;
+	}
+	
+	
+}
