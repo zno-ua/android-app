8cea7bf435c784d29aa96a26286268ba6b9cb61e
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-06-24 00:57:16 +0300

    A new type of list is applied

--- src/com/vojkovladimir/zno/TestingActivity.java
+++ src/com/vojkovladimir/zno/TestingActivity.java
@@ -1,7 +1,8 @@
 package com.vojkovladimir.zno;
 
-import android.app.ListActivity;
-import android.content.Intent;
+import java.util.ArrayList;
+
+import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -9,15 +10,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.vojkovladimir.zno.db.ZNODataBaseHelper;
+import com.vojkovladimir.zno.models.Lesson;
 
-public class TestingActivity extends ListActivity {
+public class TestingActivity extends Activity {
 
 	public static String LOG_TAG = "MyLogs";
 
-	ArrayAdapter<String> lessonsListAdapter;
+	ListView lessonsListView;
+
+	LessonsListAdapter lessonsListAdapter;
+	ArrayList<Lesson> lessonsList;
+
 	int[] lessonsIds;
 
 	OnItemClickListener itemListener = new OnItemClickListener() {
@@ -28,14 +34,14 @@ public class TestingActivity extends ListActivity {
 				Log.d(LOG_TAG, "You select lesson with id: "
 						+ lessonsIds[position]);
 
-				Intent testsList = new Intent(getApplicationContext(),
-						LessonTestsActivity.class);
-				testsList.putExtra(ZNOApplication.ExtrasKeys.ID_LESSON,
-						lessonsIds[position]);
-				testsList.putExtra(ZNOApplication.ExtrasKeys.TABLE_NAME,
-						lessonsListAdapter.getItem(position));
-
-				startActivity(testsList);
+				// Intent testsList = new Intent(getApplicationContext(),
+				// LessonTestsActivity.class);
+				// testsList.putExtra(ZNOApplication.ExtrasKeys.ID_LESSON,
+				// lessonsIds[position]);
+				// testsList.putExtra(ZNOApplication.ExtrasKeys.TABLE_NAME,
+				// lessonsListAdapter.getItem(position));
+				//
+				// startActivity(testsList);
 			} catch (ArrayIndexOutOfBoundsException e) {
 				Log.e(LOG_TAG, "You try to select lesson #" + position
 						+ " from list.");
@@ -46,21 +52,20 @@ public class TestingActivity extends ListActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_testing);
 
-		lessonsListAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1);
-
-		setListAdapter(lessonsListAdapter);
-		getListView().setOnItemClickListener(itemListener);
+		lessonsList = new ArrayList<Lesson>();
 
 		SQLiteDatabase db = ZNOApplication.getInstance().getZnoDataBaseHelper()
 				.getReadableDatabase();
 
-		Cursor c = db
-				.query(ZNODataBaseHelper.TABLE_LESSONS_LIST,
-						new String[] { ZNODataBaseHelper.KEY_ID, ZNODataBaseHelper.KEY_NAME }, null, null,
-						null, null, null);
+		Cursor c = db.query(ZNODataBaseHelper.TABLE_LESSONS_LIST, new String[] {
+				ZNODataBaseHelper.KEY_ID, ZNODataBaseHelper.KEY_NAME }, null,
+				null, null, null, null);
 
+		String lessonName;
+		int testsCounter;
+		
 		lessonsIds = new int[c.getCount()];
 
 		if (c.moveToFirst()) {
@@ -68,9 +73,21 @@ public class TestingActivity extends ListActivity {
 			int lessonNameIndex = c.getColumnIndex(ZNODataBaseHelper.KEY_NAME);
 
 			for (int i = 0; i < lessonsIds.length; i++, c.moveToNext()) {
-				lessonsListAdapter.add(c.getString(lessonNameIndex));
+				lessonName = c.getString(lessonNameIndex);
+				testsCounter = (db
+						.query("lessons_list  inner join tests_list on lessons_list.id = tests_list.id_lesson",
+								new String[] { "lessons_list.id" }, null, null,
+								null, null, null)).getCount();
+
 				lessonsIds[i] = c.getInt(lessonIdIndex);
+				lessonsList.add(new Lesson(lessonName, testsCounter));
+				Log.i(LOG_TAG, "Lesson: "+lessonName+", counts = "+testsCounter);
 			}
 		}
+
+		lessonsListAdapter = new LessonsListAdapter(this, lessonsList);
+		lessonsListView = (ListView) findViewById(R.id.lessons_list_view);
+		lessonsListView.setAdapter(lessonsListAdapter);
+		lessonsListView.setOnItemClickListener(itemListener);
 	}
 }
