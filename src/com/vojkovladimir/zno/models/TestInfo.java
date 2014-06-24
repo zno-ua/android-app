296dd81dcd80149edc45bd2878cd63c52002a3ae
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-06-24 10:50:57 +0300

    TestInfo model Added

---
+++ src/com/vojkovladimir/zno/models/TestInfo.java
@@ -0,0 +1,13 @@
+package com.vojkovladimir.zno.models;
+
+public class TestInfo {
+	String name;
+	int year;
+	int taskNum;
+
+	public TestInfo(String name, int year, int taskNum) {
+		this.name = name;
+		this.year = year;
+		this.taskNum = taskNum;
+	}
+}
