38f28db1d898a7e8cb7a33d535bc74d260ae7b44
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-07-15 22:01:55 +0300

    ViewHolder pattern is applied

--- src/com/vojkovladimir/zno/LessonsListAdapter.java
+++ src/com/vojkovladimir/zno/LessonsListAdapter.java
@@ -13,68 +13,82 @@ import com.vojkovladimir.zno.models.Lesson;
 
 public class LessonsListAdapter extends BaseAdapter {
 
-	private final Context context;
 	private final ArrayList<Lesson> list;
 	private LayoutInflater lInflater;
-	
+	private String testsOne;
+	private String testsTwoFour;
+	private String testsOverFive;
 
-	public LessonsListAdapter(Context context,  ArrayList<Lesson> list) {
-		this.list = list;
-		this.context = context;
-		lInflater = (LayoutInflater) context
-		        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	static class ViewHolder {
+		public TextView name;
+		public TextView testsCounter;
 	}
 
+	public LessonsListAdapter(Context context, ArrayList<Lesson> list) {
+		this.list = list;
+		lInflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		testsOne = context.getResources().getString(R.string.tests_one);
+		testsTwoFour = context.getResources()
+				.getString(R.string.tests_two_four);
+		testsOverFive = context.getResources().getString(
+				R.string.tests_over_five);
+	}
 
 	@Override
 	public int getCount() {
 		return list.size();
 	}
 
-
 	@Override
 	public Object getItem(int position) {
 		return list.get(position);
 	}
-
 
 	@Override
 	public long getItemId(int position) {
 		return position;
 	}
 
-
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		
-		View lessonItem = lInflater.inflate(R.layout.lessons_list_item, parent, false);
 
-		TextView name = (TextView) lessonItem
-				.findViewById(R.id.lessons_list_lesson_name);
-		TextView testsCounter = (TextView) lessonItem
-				.findViewById(R.id.lessons_list_lesson_tests_counter);
+		View lessonItem = convertView;
+
+		if (lessonItem == null) {
+			lessonItem = lInflater.inflate(R.layout.lessons_list_item, parent,
+					false);
+			ViewHolder viewHolder = new ViewHolder();
+
+			viewHolder.name = (TextView) lessonItem
+					.findViewById(R.id.lessons_list_lesson_name);
+			viewHolder.testsCounter = (TextView) lessonItem
+					.findViewById(R.id.lessons_list_lesson_tests_counter);
+			lessonItem.setTag(viewHolder);
+		}
+
+		ViewHolder viewHolder = (ViewHolder) lessonItem.getTag();
 
 		Lesson curLesson = list.get(position);
 
-		name.setText(curLesson.name);
+		viewHolder.name.setText(curLesson.name);
+
 		String counter = String.valueOf(curLesson.testsCount)+" ";
 
 		switch (curLesson.testsCount) {
 		case 1:
-			counter += context.getResources().getString(R.string.tests_one);
+			counter += testsOne;
 			break;
 		case 2:
 		case 3:
 		case 4:
-			counter += context.getResources()
-					.getString(R.string.tests_two_four);
+			counter += testsTwoFour;
 			break;
 		default:
-			counter += context.getResources().getString(
-					R.string.tests_over_five);
+			counter += testsOverFive;
 		}
 		
-		testsCounter.setText(counter);
+		viewHolder.testsCounter.setText(counter);
 
 		return lessonItem;
 	}
