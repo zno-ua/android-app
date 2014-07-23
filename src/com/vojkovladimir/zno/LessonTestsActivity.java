ada4b9ad543468e8bfb347dddb267237e8441823
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-07-23 13:17:20 +0300

    Database methods, to access information, used

--- src/com/vojkovladimir/zno/LessonTestsActivity.java
+++ src/com/vojkovladimir/zno/LessonTestsActivity.java
@@ -8,16 +8,12 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
-import com.vojkovladimir.zno.db.ZNODataBaseHelper;
 import com.vojkovladimir.zno.models.TestInfo;
 
 public class LessonTestsActivity extends Activity {
@@ -57,53 +53,18 @@ public class LessonTestsActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tests_list);
 
-		testsListView = (ListView) findViewById(R.id.tests_list_view);
-
 		Intent intent = getIntent();
-
 		String testTableName = intent
 				.getStringExtra(ZNOApplication.ExtrasKeys.TABLE_NAME);
 		int idLesson = intent.getIntExtra(ZNOApplication.ExtrasKeys.ID_LESSON,
 				-1);
-
 		setTitle(testTableName);
-
-		SQLiteDatabase db = ZNOApplication.getInstance().getZnoDataBaseHelper()
-				.getReadableDatabase();
-
-		testsList = new ArrayList<TestInfo>();
+		
+		testsList = ZNOApplication.getInstance().getZnoDataBaseHelper().getLessonTestsList(idLesson);
 		testsListAdapter = new TestsListAdapter(this, testsList);
 		testsListView = (ListView) findViewById(R.id.tests_list_view);
-
 		testsListView.setAdapter(testsListAdapter);
 		testsListView.setOnItemClickListener(itemListener);
-
-		Cursor c = db.query(ZNODataBaseHelper.TABLE_TESTS_LIST, new String[] {
-				ZNODataBaseHelper.KEY_DB_NAME, ZNODataBaseHelper.KEY_NAME_TEST,
-				ZNODataBaseHelper.KEY_YEAR, ZNODataBaseHelper.KEY_TASKS_NUM,
-				ZNODataBaseHelper.KEY_LOADED }, ZNODataBaseHelper.KEY_ID_LESSON
-				+ "=" + idLesson, null, null, null, ZNODataBaseHelper.KEY_YEAR
-				+ " DESC");
-
-		TestInfo testInfo;
-		if (c.moveToFirst()) {
-			int dbNameIndex = c.getColumnIndex(ZNODataBaseHelper.KEY_DB_NAME);
-			int nameTestIndex = c
-					.getColumnIndex(ZNODataBaseHelper.KEY_NAME_TEST);
-			int yearIndex = c.getColumnIndex(ZNODataBaseHelper.KEY_YEAR);
-			int tastsNumIndex = c
-					.getColumnIndex(ZNODataBaseHelper.KEY_TASKS_NUM);
-			int loadedIndex = c.getColumnIndex(ZNODataBaseHelper.KEY_LOADED);
-			do {
-				testInfo = new TestInfo(c.getString(dbNameIndex),
-						c.getString(nameTestIndex), c.getInt(yearIndex),
-						c.getInt(tastsNumIndex),
-						(c.getInt(loadedIndex) == 0) ? false : true);
-				testsList.add(testInfo);
-			} while (c.moveToNext());
-		}
-
-		Log.i(LOG_TAG, getTitle() + " has " + testsList.size() + " tests");
 	}
 
 	private Dialog onDialogCreate(int id) {
--- src/com/vojkovladimir/zno/TestingActivity.java
+++ src/com/vojkovladimir/zno/TestingActivity.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,7 +11,6 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
-import com.vojkovladimir.zno.db.ZNODataBaseHelper;
 import com.vojkovladimir.zno.models.Lesson;
 
 public class TestingActivity extends Activity {
@@ -32,13 +29,10 @@ public class TestingActivity extends Activity {
 		public void onItemClick(AdapterView<?> parent, View v, int position,
 				long id) {
 			try {
-				Log.d(LOG_TAG, "You select lesson with id: "
-						+ lessonsIds[position]);
-
 				Intent testsList = new Intent(getApplicationContext(),
 						LessonTestsActivity.class);
 				testsList.putExtra(ZNOApplication.ExtrasKeys.ID_LESSON,
-						lessonsIds[position]);
+						lessonsList.get(position).id);
 				testsList.putExtra(ZNOApplication.ExtrasKeys.TABLE_NAME,
 						lessonsList.get(position).name);
 
@@ -55,39 +49,7 @@ public class TestingActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_testing);
 
-		lessonsList = new ArrayList<Lesson>();
-
-		SQLiteDatabase db = ZNOApplication.getInstance().getZnoDataBaseHelper()
-				.getReadableDatabase();
-
-		Cursor c = db.query(ZNODataBaseHelper.TABLE_LESSONS_LIST, new String[] {
-				ZNODataBaseHelper.KEY_ID, ZNODataBaseHelper.KEY_NAME }, null,
-				null, null, null, null);
-
-		String lessonName;
-		int testsCounter;
-
-		lessonsIds = new int[c.getCount()];
-
-		if (c.moveToFirst()) {
-			int lessonIdIndex = c.getColumnIndex(ZNODataBaseHelper.KEY_ID);
-			int lessonNameIndex = c.getColumnIndex(ZNODataBaseHelper.KEY_NAME);
-
-			for (int i = 0; i < lessonsIds.length; i++, c.moveToNext()) {
-				lessonName = c.getString(lessonNameIndex);
-				lessonsIds[i] = c.getInt(lessonIdIndex);
-				
-				testsCounter = (db.query(ZNODataBaseHelper.TABLE_TESTS_LIST,
-						new String[] { ZNODataBaseHelper.KEY_ID }, ZNODataBaseHelper.KEY_ID_LESSON+" = "+lessonsIds[i], null,
-						null, null, null)).getCount();
-
-				
-				lessonsList.add(new Lesson(lessonName, testsCounter));
-				Log.i(LOG_TAG, "Lesson: " + lessonName + ", counts = "
-						+ testsCounter);
-			}
-		}
-
+		lessonsList = ZNOApplication.getInstance().getZnoDataBaseHelper().getLessonsList();
 		lessonsListAdapter = new LessonsListAdapter(this, lessonsList);
 		lessonsListView = (ListView) findViewById(R.id.lessons_list_view);
 		lessonsListView.setAdapter(lessonsListAdapter);
