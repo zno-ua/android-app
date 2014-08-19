ff8710147d25b96bb6d7da521bbe08dc03061035
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-08-19 17:47:45 +0300

    Adding constants of question types  to the question model

--- src/com/vojkovladimir/zno/models/Question.java
+++ src/com/vojkovladimir/zno/models/Question.java
@@ -2,6 +2,12 @@ package com.vojkovladimir.zno.models;
 
 public class Question {
 
+	public static int TYPE_1 = 1;
+	public static int TYPE_2 = 2;
+	public static int TYPE_3 = 3;
+	public static int TYPE_4 = 4;
+	public static int TYPE_5 = 5;
+
 	public int id;
 	public int idTestQuestion;
 	public String question;
