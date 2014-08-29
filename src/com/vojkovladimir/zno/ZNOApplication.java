9e7da7c2841409baf96b677a21420c856d5bc25b
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-08-30 00:26:49 +0300

    Added method for building app logo

--- src/com/vojkovladimir/zno/ZNOApplication.java
+++ src/com/vojkovladimir/zno/ZNOApplication.java
@@ -1,8 +1,14 @@
 package com.vojkovladimir.zno;
 
 import android.app.Application;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.text.SpannableString;
 import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -69,5 +75,20 @@ public class ZNOApplication extends Application {
 			mRequestQueue.cancelAll(tag);
 		}
 	}
+	
+	public static void buildLogo(TextView logoTextView,Resources resources,AssetManager assets){
+		String appName = resources.getString(R.string.app_name);
+		String year = "2015";
+		String logoText = appName+" "+year;
+		
+		SpannableString spannableString = new SpannableString(logoText);
+		spannableString.setSpan(new ForegroundColorSpan(resources.getColor(R.color.blue_light)), 0, appName.length(), 0);
+		spannableString.setSpan(new ForegroundColorSpan(resources.getColor(R.color.quote_color)), appName.length()+1, logoText.length(), 0);
+		
+		Typeface ptSansBold = Typeface.createFromAsset(assets, "fonts/PT_Sans-Web-Bold.ttf");
+		
+		logoTextView.setText(spannableString);
+		logoTextView.setTypeface(ptSansBold);
+	};
 
 }
