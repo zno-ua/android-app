d612361d045b41b8e6c3bdf6bbb9dda28b8bbc1b
vojkovladimir <voiko.vladimir.zp@gmail.com>
2014-08-30 13:19:48 +0300

    SplashActivity created

---
+++ src/com/vojkovladimir/zno/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.vojkovladimir.zno;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+public class SplashActivity extends Activity {
+
+	private static int SPLASH_TIME_OUT = 3000;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash);
+
+		TextView logo;
+		logo = (TextView) findViewById(R.id.app_logo);
+		ZNOApplication.buildLogo(logo, getResources(), getAssets());
+
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				Intent i = new Intent(SplashActivity.this, MainActivity.class);
+				startActivity(i);
+				finish();
+			}
+		}, SPLASH_TIME_OUT);
+	}
+
+}
