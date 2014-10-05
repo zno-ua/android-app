2e8355bf4ad28cd9a03beb11918c3daffe926975
Vladimir <vojkovladimir@gmail.com>
2014-10-05 14:33:15 +0300

    Remove Splash

--- src/com/vojkovladimir/zno/MainActivity.java
+++ src/com/vojkovladimir/zno/MainActivity.java
@@ -1,16 +1,25 @@
 package com.vojkovladimir.zno;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.widget.TextView;
 
+import com.vojkovladimir.zno.db.ZNODataBaseHelper;
+import com.vojkovladimir.zno.fragments.TestTimerFragment;
+import com.vojkovladimir.zno.models.Test;
+
 import java.util.Random;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
+    ZNODataBaseHelper db;
     String[] quotes;
     String quoteTitle;
     TextView quote;
@@ -26,6 +35,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         ZNOApplication.buildLogo(logo, getResources(), getAssets());
         quotes = getResources().getStringArray(R.array.quotes_2011);
         quoteTitle = getResources().getString(R.string.quotes_2011_title);
+        db = ZNOApplication.getInstance().getZnoDataBaseHelper();
         findViewById(R.id.begin_testing_btn).setOnClickListener(this);
     }
 
@@ -34,6 +44,49 @@ public class MainActivity extends Activity implements View.OnClickListener {
         refreshQuote();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final SharedPreferences preferences = getSharedPreferences(ZNOApplication.APP_SETTINGS, Context.MODE_PRIVATE);
+        if (preferences.contains(TestActivity.Extra.USER_ANSWERS_ID)) {
+            final int userAnswersId = preferences.getInt(TestActivity.Extra.USER_ANSWERS_ID, -1);
+            final int questionNumber = preferences.getInt(TestActivity.Extra.QUESTION_NUMBER, -1);
+            final int testId = preferences.getInt(Test.TEST_ID, -1);
+
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            dialogBuilder.setMessage(getString(R.string.unfinished_test_alert_text));
+            dialogBuilder.setPositiveButton(R.string.dialog_positive_text, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent startedActivity;
+                    startedActivity = new Intent(TestActivity.Action.CONTINUE_PASSAGE_TEST);
+                    startedActivity.putExtra(Test.TEST_ID, testId);
+                    startedActivity.putExtra(TestActivity.Extra.USER_ANSWERS_ID, userAnswersId);
+                    startedActivity.putExtra(TestActivity.Extra.QUESTION_NUMBER, questionNumber);
+                    if (preferences.contains(TestTimerFragment.MILLIS_LEFT)) {
+                        startedActivity.putExtra(TestTimerFragment.MILLIS_LEFT, preferences.getLong(TestTimerFragment.MILLIS_LEFT, -1));
+                    }
+                    startActivity(startedActivity);
+                }
+            });
+            dialogBuilder.setNegativeButton(R.string.dialog_negative_text, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    db.deleteUserAnswers(userAnswersId);
+                    SharedPreferences preferences = getSharedPreferences(ZNOApplication.APP_SETTINGS, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.remove(Test.TEST_ID);
+                    editor.remove(TestActivity.Extra.USER_ANSWERS_ID);
+                    editor.remove(TestActivity.Extra.QUESTION_NUMBER);
+                    if (preferences.contains(TestTimerFragment.MILLIS_LEFT)) {
+                        editor.remove(TestTimerFragment.MILLIS_LEFT);
+                    }
+                    editor.apply();
+                }
+            });
+            dialogBuilder.create().show();
+        }
+    }
 
     public void refreshQuote() {
         String text = quoteTitle + "<br>";
