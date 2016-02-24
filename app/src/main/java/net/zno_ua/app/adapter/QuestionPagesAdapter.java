73853cf9de0a71be1ba7163c0e561c3b115cb844
vojkovladimir <vojkovladimir@gmail.com>
2016-02-24 14:54:55 +0200

    Add statement tabs title

--- app/src/main/java/net/zno_ua/app/adapter/QuestionPagesAdapter.java
+++ app/src/main/java/net/zno_ua/app/adapter/QuestionPagesAdapter.java
@@ -5,16 +5,18 @@ import android.support.v4.app.Fragment;
 
 import net.zno_ua.app.R;
 import net.zno_ua.app.model.TestingInfo;
-import net.zno_ua.app.provider.Query;
 import net.zno_ua.app.fragment.QuestionFragment;
 
-import static net.zno_ua.app.provider.ZNOContract.Subject.ENGLISH;
+import static net.zno_ua.app.provider.ZNOContract.Question.TYPE_2;
+import static net.zno_ua.app.provider.ZNOContract.Subject.*;
+import static net.zno_ua.app.provider.Query.Question.Column;
 
 /**
  * @author vojkovladimir.
  */
 public class QuestionPagesAdapter extends CursorFragmentStatePagerAdapter {
     private final String QUESTION_NUMBER_FORMAT;
+    private final String STATEMENT_QUESTION;
     private final long mSubjectId;
 
     public QuestionPagesAdapter(Fragment parentFragment, TestingInfo testingInfo) {
@@ -22,26 +24,32 @@ public class QuestionPagesAdapter extends CursorFragmentStatePagerAdapter {
         mSubjectId = testingInfo.getSubjectId();
         if (mSubjectId == ENGLISH) {
             QUESTION_NUMBER_FORMAT = parentFragment.getString(R.string.question_en_format);
+            STATEMENT_QUESTION = parentFragment.getString(R.string.statement_en);
         } else {
             QUESTION_NUMBER_FORMAT = parentFragment.getString(R.string.question_format);
+            STATEMENT_QUESTION = parentFragment.getString(R.string.statement);
         }
     }
 
     @Override
     public Fragment getItem(int position, Cursor cursor) {
         return QuestionFragment.newInstance(
-                cursor.getInt(Query.Question.Column.ID),
-                cursor.getInt(Query.Question.Column.TYPE),
-                cursor.getString(Query.Question.Column.TEXT),
-                cursor.getString(Query.Question.Column.ADDITIONAL_TEXT),
-                cursor.getString(Query.Question.Column.ANSWERS),
-                cursor.getInt(Query.Question.Column.POINT),
-                cursor.getString(Query.Question.Column.CORRECT_ANSWER)
+                cursor.getInt(Column.ID),
+                cursor.getInt(Column.TYPE),
+                cursor.getString(Column.TEXT),
+                cursor.getString(Column.ADDITIONAL_TEXT),
+                cursor.getString(Column.ANSWERS),
+                cursor.getInt(Column.POINT),
+                cursor.getString(Column.CORRECT_ANSWER)
         );
     }
 
     @Override
     public CharSequence getPageTitle(int position, Cursor cursor) {
+        if ((mSubjectId == ENGLISH || mSubjectId == UKRAINIAN)
+                && cursor.getInt(Column.TYPE) == TYPE_2 && cursor.getCount() - 1 == cursor.getPosition()) {
+            return STATEMENT_QUESTION;
+        }
         return String.format(QUESTION_NUMBER_FORMAT, (position + 1));
     }
 
